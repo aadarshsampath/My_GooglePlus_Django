b143d65336865d71c0fdae65b1669f94f1919677
Aadarsh <aadarsh.sampath@gmail.com>
2016-09-20 14:02:01 -0700

    Defining the account manager class.

--- DjangoGplus/DjangoGplus/authentication/models.py
+++ DjangoGplus/DjangoGplus/authentication/models.py
@@ -2,9 +2,36 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import AbstractBaseUser
 
+from django.contrib.auth.models import BaseUserManager
+
 from django.db import models
 
 # Create your models here.
+
+class AccountManager(BaseUserManager):
+	def create_user(self,email,password=None, **kwargs):
+		if not email:
+			raise ValueError('Users must have a valid email address')
+
+		if not kwargs.get('username'):
+			raise ValueError('Users must have a valid username')
+
+		account = self.model(
+			email=self.normalize_email(email), username=kwargs.get('username'))
+
+		account.set_password(password)
+		account.save()
+
+		return account
+
+	def create_superuser(self,email,password, **kwargs):
+		account=self.create_user(email,password, **kwargs)
+
+		account.is_admin = True
+		account.save()
+
+		return account
+
 
 #AbstractBaseUser is DjangoDefined class to get all the related fields
 # for a base user.
