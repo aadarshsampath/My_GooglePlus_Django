ed7f6af39965cd9bf04669cf33dae2c1554dd513
Aadarsh <aadarsh.sampath@gmail.com>
2016-09-20 14:13:12 -0700

    Adding accounts model to the database

--- DjangoGplus/DjangoGplus/authentication/models.py
+++ DjangoGplus/DjangoGplus/authentication/models.py
@@ -45,28 +45,19 @@ class Account(AbstractBaseUser):
 
 	first_name = models.CharField(max_length=40, blank=True)
 	last_name = models.CharField(max_length=40, blank=True)
-    tagline = models.CharField(max_length=140, blank=True)
-
-
-    is_admin= models.BooleanField(default=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    # For getting a model instance in Django we need to Give it in the
-    # format <className>Manager(). to instantiate an object. 
-    # similar to Java obj a = new obj()
-    objects = AccountManager()
-
-    # Treat the email field to be the username (This is a unique field)
-    USERNAME_FIELD = 'email' 
-    REQUIRED_FIELDS = ['username']
-
-    def __unicode__(self):
-    	return self.email
-
-    def get_full_name(self):
-    	return ' '.join([self.first_name,self.last_name])
-
-    def get_short_name(self):
-    	return self.first_name
+	tagline = models.CharField(max_length=140, blank=True)
+	is_admin= models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	# For getting a model instance in Django we need to Give it in the
+	# format <className>Manager(). to instantiate an object. 
+	# similar to Java obj a = new obj()
+	objects = AccountManager()
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
+	def __unicode__(self):
+		return self.email
+	def get_full_name(self):
+		return ' '.join([self.first_name,self.last_name])
+	def get_short_name(self):
+		return self.first_name
